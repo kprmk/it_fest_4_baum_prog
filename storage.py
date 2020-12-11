e20dd4ec444a6c26e8a2dfa4767239fa293b5453
Kirill Priyomko <47541069+kprmk@users.noreply.github.com>
2020-12-11 23:20:09 +0300

    fix database

--- storage.py
+++ storage.py
@@ -1,27 +1,25 @@
-import sqlite3
-
+import sqlite3 # structered query language
+# реляционная база данных (relation)
 
 class Storage:
-	def __init__(self, name='data.db'):
-		self.nameDB = name
-		self.cnct = sqlite3.connect(self.nameDB)
-		self.crs = self.cnct.cursor()
+	def __init__(self, name='data.db'): #конструктор, вызывается при создании
+		self.nameDB = name # name Data Base
+		self.cnct = sqlite3.connect(name) # connect
+		self.crs = self.cnct.cursor() # crs - cursor
 		self.create_table()
 
-	def create_table(self, name='init'):
-		self.crs = self.cnct.execute(f'''CREATE TABLE IF NOT EXISTS '{name}' (
+	def create_table(self):
+		self.crs = self.cnct.execute('''CREATE TABLE IF NOT EXISTS score_table (
 											name text, score int		)''')
 
-	def insert_into(self, name_usr, score, name='init'):
-		self.crs.execute(f'''INSERT INTO {name} (name, score) VALUES('{name_usr}', '{score}')''')
+	def insert_into(self, name_usr, score):
+		self.crs.execute(f'''INSERT INTO score_table (name, score) 
+								VALUES('{name_usr}', '{score}')''')
 		self.cnct.commit()
 
-	def select_all(self, name='init'):
-		self.crs.execute(f'''SELECT * FROM {name} ''')
+	def select_all(self):
+		self.crs.execute('''SELECT * FROM score_table ''')
 		return self.crs.fetchall()
 
-	def del_table(self, name='init'):
-		self.crs.execute(f'''DROP TABLE '{name}' ''')
-
-	def __del__(self):
+	def __del__(self):# деструктор, вызывается при уничтожении
 		self.cnct.close()
